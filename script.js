309cce9e67a9c5c242a65ab9879e15fd773a140a
Jordan Todorovski <jordantodorovski@Jordans-MacBook-Air.local>
2025-02-28 20:17:27 +0100

    second version of website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".acc-title").forEach(button => {
+document.querySelectorAll(".card-header").forEach(button => {
     button.addEventListener("click", function () {
         let target = document.querySelector(this.getAttribute("data-target"));
         target.classList.toggle("show");
