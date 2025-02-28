a79768944e730fd3a5a75cd0cb7789915be7f50d
Jordan Todorovski <jordantodorovski@Jordans-MacBook-Air.local>
2025-02-28 23:27:25 +0100

    Nothing added, tried sliders for skills but i didnt liked it...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll(".card-header").forEach(button => {
-    button.addEventListener("click", function () {
-        let target = document.querySelector(this.getAttribute("data-target"));
-        target.classList.toggle("show");
-    });
+document.querySelectorAll(".card-header").forEach((button) => {
+  button.addEventListener("click", function () {
+    let target = document.querySelector(this.getAttribute("data-target"));
+    target.classList.toggle("show");
+  });
 });
